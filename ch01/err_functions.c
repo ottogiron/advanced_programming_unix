2ee54cae3cc7de1003f540340d8d961d5a8a4d26
Otto Giron <ottog2486@gmail.com>
2018-09-12 02:42:55 -0600

    add err_functions.c

---
+++ ch01/err_functions.c
@@ -0,0 +1,11 @@
+#include "apue.h"
+#include <errno.h>
+
+int
+main(int argc, char const *argv[])
+{
+    fprintf(stderr, "EACCESS: %s\n", strerror(EACCES));
+    errno = ENOENT;
+    perror(argv[0]);
+    return 0;
+}
